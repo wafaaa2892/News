aaa97ef4497ab4653b9c0ef77e3cdb3b5bce22d5
wafaaa2892 <wafaaa2892@gmail.com>
2024-07-29 20:58:06 +0300

    Deployed website

--- src/component/NotFound/NotFound.jsx
+++ src/component/NotFound/NotFound.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import styles from './NotFound.module.css';
+// import styles from './NotFound.module.css';
 export default function NotFound() {
     return <>
-        <h1 className="mt-5 text-center text-white">
+        <h1 className="mt-5 text-center text-white vh-100">
             Ooops!!
             this page is not found
         </h1>
